55cd2922bc2936faa320cf0b46d962e80228aca0
Ines <inesoubabas56@gmail.com>
2025-09-15 13:13:45 +0200

    all ripos fixe

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -3,10 +3,22 @@
 
 #include <stddef.h>
 
-/* Provided function */
+/**
+ * print_array - Print an array of integers
+ * @array: Pointer to the first element
+ * @size: Number of elements in @array
+ *
+ * Return: void
+ */
 void print_array(const int *array, size_t size);
 
-/* Your function */
+/**
+ * merge_sort - Sort an array of integers in ascending order using Merge sort
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in @array
+ *
+ * Return: void
+ */
 void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
